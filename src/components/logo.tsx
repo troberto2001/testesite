a3e105c4e6edba2d951020be238909f4408a0eb2
Tiago R. <tiagoroberto2001@gmail.com>
2025-11-18 19:39:20 +0000

    remove logo image, keept it simple limpatelhados

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,7 @@
 import type { SVGProps } from "react";
-import Image from "next/image";
 
 const Logo = (props: SVGProps<SVGSVGElement>) => (
   <div className="flex items-center gap-2" >
-    <Image
-        src="https://i.imgur.com/bQjt3jT.png"
-        alt="LimpaTelhados Logo"
-        width={32}
-        height={32}
-        className="text-primary"
-      />
     <span className="font-headline text-xl font-bold">LimpaTelhados</span>
   </div>
 );
