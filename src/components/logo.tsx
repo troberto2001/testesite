3fd7fc9416a20262e8f43ca6e75a62910b83a26e
Tiago R. <tiagoroberto2001@gmail.com>
2025-11-18 19:03:09 +0000

    add this image instead https://imgur.com/a/fkzWUkD (_for element div_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,26 +1,15 @@
 import type { SVGProps } from "react";
+import Image from "next/image";
 
 const Logo = (props: SVGProps<SVGSVGElement>) => (
   <div className="flex items-center gap-2" >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="text-primary"
-      {...props}
-    >
-      <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-      <polyline points="9 22 9 12 15 12 15 22" />
-      <path d="m18 15-3-3-3 3" />
-      <path d="m19 12-2-2" />
-      <path d="m13 18-1-1" />
-    </svg>
+    <Image
+        src="https://i.imgur.com/gO1Xg9P.png"
+        alt="LimpaTelhados Logo"
+        width={32}
+        height={32}
+        className="text-primary"
+      />
     <span className="font-headline text-xl font-bold">LimpaTelhados</span>
   </div>
 );
