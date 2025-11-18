cb6ef8b02769977275734dc6e4c7652cec838fb6
Tiago R. <tiagoroberto2001@gmail.com>
2025-11-18 19:27:01 +0000

    add this image https://sunnypressurewashing.com/wp-content/uploads/2022/

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sunnypressurewashing.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
