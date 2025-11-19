4aaa6a67838724d6e9e2a8d8479445e3c965179b
Tiago R. <tiagoroberto2001@gmail.com>
2025-11-19 09:10:58 +0000

    add LimpaTelhado text (its cuted) (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,27 +1,36 @@
 import type { SVGProps } from "react";
 
 const Logo = ({ width = 150, ...props }: SVGProps<SVGSVGElement> & { width?: number }) => {
-  const aspectRatio = 150 / 40;
+  const aspectRatio = 150 / 110;
   const height = width / aspectRatio;
 
   return (
     <svg
       width={width}
       height={height}
-      viewBox="0 0 150 40"
+      viewBox="0 0 150 110"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-        <text
-            x="50%"
-            y="50%"
-            dominantBaseline="middle"
-            textAnchor="middle"
-            style={{ fill: 'hsl(var(--primary))', fontFamily: 'Cormorant Garamond, serif', fontSize: '28px', fontWeight: '700', letterSpacing: '0.05em' }}
-        >
-            LimpaTelhados
-        </text>
+      <path
+        d="M86.3217 55.4371L46.3379 75.6455L50.158 43.2555L24.3379 23.0471L55.4484 22.2824L65.5118 0.437119L84.814 26.5471L114.422 17.5471L104.358 45.419L129.422 60.4371L98.3113 63.4824L86.3217 92.591L72.4913 65.6455L86.3217 55.4371Z"
+        fill="hsl(var(--primary))"
+      />
+      <text
+        x="50%"
+        y="95%"
+        dominantBaseline="text-after-edge"
+        textAnchor="middle"
+        style={{
+          fill: "hsl(var(--foreground))",
+          fontFamily: "Cormorant Garamond, serif",
+          fontSize: "24px",
+          fontWeight: "700",
+        }}
+      >
+        LimpaTelhados
+      </text>
     </svg>
   );
 };
